bf39408d057fa9ce0a1cfd78aaa8e962493b7bdf
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-07-11 01:08:56 +0100

    terminado

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 
 let active = 0
@@ -17,8 +18,7 @@ function setSlider() {
 
     let itemOld = container.querySelector('.list .item.active')
     itemOld.classList.remove('active')
-
-
+    
     let dotsOld = indicator.querySelector('ul li.active')
     dotsOld.classList.remove('active')
     dots[active].classList.add('active')
@@ -31,17 +31,16 @@ function setSlider() {
 
 
 nextButton.onclick = () => {
-
-
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
     setSlider()
     items[active].classList.add('active')
-
-
 }
 
 
 prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+
     active = active - 1 < firstPosition ? lastPosition : active - 1
     setSlider()
     items[active].classList.add('active')
